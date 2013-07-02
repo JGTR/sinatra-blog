8116c0fb06f2a11a147a65da272eeb416b0a2cba
Jordan Trevino <jordan.trevino@gmail.com>
2013-07-02 17:55:34 -0400

    first

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sqlite3'
+gem 'rake'
+
+gem 'activerecord'
+gem 'sinatra-activerecord'
+
+group :development do
+  gem "shotgun"
+  gem "tux"
+end
---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+set :database, "sqlite3:///blog.db"
+
+class Post < ActiveRecord::Base
+end
+
+module SinatraBlog
+  class App < Sinatra::Base
+
+  end
+end
+
